e8d22932c8f7e653cfda8a7fe50c3712bf509fea
AlonaRahilevych <ragilevic@gmail.com>
2025-05-05 11:45:26 +0200

    feat: create user schema

---
+++ src/models/User.js
@@ -0,0 +1,36 @@
+import mongoose from 'mongoose';
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 3,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    validate: {
+      validator: function (value) {
+        return /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(value);
+      },
+      message: (props) => `${props.value} is not a valid email address!`,
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ['user', 'admin'],
+    default: 'user',
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+const User = mongoose.model('User', userSchema);
+
+export default User;
