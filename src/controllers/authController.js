ada28e9adcb6ba643ea58df18802a130a495290d
AlonaRahilevych <ragilevic@gmail.com>
2025-05-13 21:57:42 +0200

    feat: add logout

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -11,12 +11,28 @@ export const register = async (req, res, next) => {
 export const login = async (req, res, next) => {
   try {
     const { token, user } = await authService.loginUser(req.body);
+    console.log('token', token);
     res.cookie('token', token, {
       httpOnly: true,
       secure: false,
       sameSite: 'Strict',
+      path: '/',
     });
     res.status(200).json(user);
+  } catch (error) {
+    next(error);
+  }
+};
+export const logoutUser = (req, res, next) => {
+  try {
+    res.clearCookie('token', {
+      httpOnly: true,
+      secure: false,
+      sameSite: 'Strict',
+      path: '/',
+    });
+
+    return res.status(200).json({ message: 'Logged out successfully' });
   } catch (error) {
     next(error);
   }
