46e4f0d772374b66cdced246404d10e081d0e9e5
AlonaRahilevych <ragilevic@gmail.com>
2025-05-14 07:45:42 +0200

    chore:update dependencies

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ import cookieParser from 'cookie-parser';
 dotenv.config();
 
 const app = express();
-
+app.use(cookieParser());
 app.use(helmet());
-app.use(cors({ origin: 'http://localhost:5174', credentials: true }));
+app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(cookieParser());
