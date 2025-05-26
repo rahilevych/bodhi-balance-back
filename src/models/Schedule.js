50c3b199fb1dc56675d54c484eceba55bc40c5fc
AlonaRahilevych <ragilevic@gmail.com>
2025-05-26 22:56:53 +0200

    feat: add schema for schedule

---
+++ src/models/Schedule.js
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose';
+
+const scheduleSchema = new mongoose.Schema({
+  datetime: {
+    type: Date,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+
+  spots_taken: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  spots_total: {
+    type: Number,
+    required: true,
+  },
+  trainer_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    // ref: 'Trainer',
+    required: true,
+  },
+  yogaStyle_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    // ref: 'YogaStyle',
+    required: true,
+  },
+});
+
+const Schedule = mongoose.model('Schedule', scheduleSchema);
+export default Schedule;
