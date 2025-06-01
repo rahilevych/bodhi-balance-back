1d60be195b3aa8ba1e8f54ba8a568468d5cbecfe
AlonaRahilevych <ragilevic@gmail.com>
2025-06-01 17:00:37 +0200

    refactor:delete unused  imports

--- src/routes/scheduleRoutes.js
+++ src/routes/scheduleRoutes.js
@@ -1,7 +1,8 @@
 import express from 'express';
-import { get } from 'mongoose';
+
 import { getAllTrainingsForDate } from '../controllers/scheduleController.js';
 
 const scheduleRouter = express.Router();
 scheduleRouter.get('/trainings', getAllTrainingsForDate);
+
 export default scheduleRouter;
