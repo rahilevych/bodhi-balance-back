ac3d036b305850678c7f0151c33d0c4927a8be69
AlonaRahilevych <ragilevic@gmail.com>
2025-06-04 19:18:12 +0200

    feat: add model for plans

---
+++ src/models/Plan.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+const planSchema = new mongoose.Schema({
+  type: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+});
+const Plan = mongoose.model('plan', planSchema);
+export default Plan;
