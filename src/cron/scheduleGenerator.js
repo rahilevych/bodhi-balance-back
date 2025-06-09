ae1c832800e3d8af3b2b815d6c249247501ab029
AlonaRahilevych <ragilevic@gmail.com>
2025-06-09 19:29:47 +0200

    refactor:change time for generation schedule

--- src/cron/scheduleGenerator.js
+++ src/cron/scheduleGenerator.js
@@ -2,7 +2,7 @@ import cron from 'node-cron';
 import connectDB from '../config/db.js';
 import { generateWeeklySchedule } from '../utils/generateSchedule.js';
 
-cron.schedule('0 3 * * *', async () => {
+cron.schedule('0 20 * * *', async () => {
   const DB_URI = process.env.DB_URI;
   try {
     connectDB(DB_URI);
