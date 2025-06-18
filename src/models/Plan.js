7a92576bcfb02b5612c852ea90810cbee3790d8b
AlonaRahilevych <ragilevic@gmail.com>
2025-06-18 23:43:34 +0200

    feat:update Plan schema

--- src/models/Plan.js
+++ src/models/Plan.js
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 const planSchema = new mongoose.Schema({
   type: {
     type: String,
+    enum: ['pack', 'unlimited'],
     required: true,
   },
   title: {
@@ -20,6 +21,18 @@ const planSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  durationInMonths: {
+    type: Number,
+    required: function () {
+      return this.type === 'unlimited';
+    },
+  },
+  trainingsCount: {
+    type: Number,
+    required: function () {
+      return this.type === 'pack';
+    },
+  },
 });
-const Plan = mongoose.model('plan', planSchema);
+const Plan = mongoose.model('Plan', planSchema);
 export default Plan;
