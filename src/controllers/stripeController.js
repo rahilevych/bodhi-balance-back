42309234d13f5eaf15364b193103275992027630
AlonaRahilevych <ragilevic@gmail.com>
2025-06-18 23:47:45 +0200

    feat:  provide metadata

--- src/controllers/stripeController.js
+++ src/controllers/stripeController.js
@@ -1,14 +1,19 @@
+import { createBookingAfterPayment } from '../services/bookingService.js';
 import * as stripeService from '../services/stripeService.js';
+import { createSubscriptionAfterPayment } from '../services/subscriptionService.js';
 
 export const handleStripeWebhook = async (req, res) => {
   try {
     const event = stripeService.verifyStripeEvent(req);
     if (event.type === 'checkout.session.completed') {
       const session = event.data.object;
-      const { userId, trainingId } = session.metadata;
-      await stripeService.createBookingAfterPayment(userId, trainingId);
+      const { userId, productId, type } = session.metadata;
+      if (type === 'training') {
+        await createBookingAfterPayment(userId, productId);
+      } else if (type === 'subscription') {
+        await createSubscriptionAfterPayment(userId, productId);
+      }
     }
-
     res.status(200).json({ received: true });
   } catch (err) {
     console.error('Webhook error:', err);
